c82dcb176ca0e84bcb85e2b52a98646abddd47e3
saleh <salehassadi8731@gmail.com>
2026-03-24 14:45:45 -0600

    Psuedo code added in network class

--- NetworkGen/network.m
+++ NetworkGen/network.m
@@ -19,7 +19,19 @@ properties
         'Ly', 10, ...
         'Lz', 10, ...
         'scale', 1, ...
-        'boundary', 'fixed' ...
+        'boundary', 'fixed', ...
+        'xlo', [], ...
+        'xhi', [], ...
+        'ylo', [], ...
+        'yhi', [], ...
+        'zlo', [], ...
+        'zhi', [], ...
+        'Max_atom', [], ...
+        'Max_bond', [], ...
+        'node_scatter_max_tries', [], ...
+        'bond_global_try_limit', [], ...
+        'max_attempts_without_progress', [], ...
+        'max_tries_per_node_sample', [] ...
     );
 
     %%% Architecture subclass
@@ -27,6 +39,7 @@ properties
 
     %%% Per/atom
     peratom = struct(...
+        'Max_peratom_bond',[] ...
     );
 
     %%% Per/bond subclass
@@ -59,37 +72,79 @@ methods
         %%% Loop over replicates
         for ii=1 %Nreps
 
-            % -----------------------------------%
-            %1. Prepare replicate-specific information
+            % ---------------------------------------------------------
+            % 1. Prepare replicate-specific information
+            % ---------------------------------------------------------
+            obj = localPrepareReplicate(obj, ii, Nreplicates);
 
-            %2. Constuct domain
-
-            %3. Add atoms
-
-            %4. Assign per/atom
-
-            %5. Add bonds
-
-            %6. Assign per/bond
-
-            %7. Add defects
-
-            %8. Clean-up network
-
-
-            % -----------------------------------%
-            %9. Construct local density potential
-
-            %10. Scale domain if needed
+            % ---------------------------------------------------------
+            % 2. Construct domain
+            % ---------------------------------------------------------
+            SetupDomain(obj);
+            % New version of SetupDomain should read from obj.domain, % obj.arch, ...
             
-            % -----------------------------------%
-            %11. Show visualization and statistics
+            % ---------------------------------------------------------
+            % 3. Add atoms
+            % ---------------------------------------------------------
+            Atoms = AddAtoms(obj);
 
-            %12. Computes
+            % ---------------------------------------------------------
+            % 4. Assign per/atom
+            % ---------------------------------------------------------
+            Atoms = AssignPerAtom(obj, Atoms);
+            % AssignPerAtom should read per-atom settings (if any) from obj
+            % Decides internally to do random or hex based on geometry flag
 
-            %13. Write data files
+            % ---------------------------------------------------------
+            % 5. Add bonds
+            % ---------------------------------------------------------
+            [Atoms, Bonds] = AddBonds(obj, Atoms);
 
-            % -----------------------------------%
+            % ---------------------------------------------------------
+            % 6. Assign per/bond
+            % ---------------------------------------------------------
+            Nvec = AssignPerBond(obj, Bonds, Atoms);
+            % AssignPerBond should read obj.perbond.* settings and assign
+
+            % ---------------------------------------------------------
+            % 7. Add defects
+            % ---------------------------------------------------------
+            [Atoms, Bonds] = AddDefects(obj, Atoms, Bonds, Nvec);
+            % AddDefects should read obj.defect
+
+            % ---------------------------------------------------------
+            % 8. Clean-up network
+            % ---------------------------------------------------------
+            [Atoms, Bonds, Nvec] = CleanupNetwork(obj, Atoms, Bonds, Nvec);
+            % CleanupNetwork can prune isolated nodes, rebuild connectivity,
+            % update Nvec, etc.
+
+
+            % ---------------------------------------------------------
+            % 9. Construct local density potential
+            % ---------------------------------------------------------
+            LDpot = ConstructLDPotential(obj, Atoms, Bonds, Nvec);
+
+            % ---------------------------------------------------------
+            % 10. Scale domain if needed
+            % ---------------------------------------------------------
+            [Atoms, Bonds] = ScaleDomain(obj, Atoms, Bonds);
+            
+            % ---------------------------------------------------------
+            % 11. Show visualization and statistics
+            % ---------------------------------------------------------
+            VisualizeNetwork(obj, Atoms, Bonds, Nvec);
+            % VisualizeNetwork should check obj.flags.iplot internally
+
+            % ---------------------------------------------------------
+            % 12. Computes
+            % ---------------------------------------------------------
+            order = ComputeOrder(obj, Atoms, Bonds);
+
+            % ---------------------------------------------------------
+            % 13. Write data files
+            % ---------------------------------------------------------
+            WriteDataFiles(obj, Atoms, Bonds, Nvec, LDpot, order);
 
         end
 
